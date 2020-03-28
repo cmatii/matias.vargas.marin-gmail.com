e1d7dce88fde1fc3658644b8a5b8bc2eb9f4843f
cmatii <matias.vargas.marin@gmail.com>
2020-03-28 00:02:06 -0300

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Index Hola Mundo!"
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/members")
+def members():
+    return "Members"
+
+
+if __name__ == "__main__":
+    app.run()
